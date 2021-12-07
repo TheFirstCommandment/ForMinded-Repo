1ed044a5497da979334e7d00463e236bec4e2565
Mattias_Nelsen <vbleise@mail.ru>
2021-12-07 15:00:33 +0400

    Reverse (without fucntion)

---
+++ fib.py
@@ -0,0 +1,38 @@
+
+s1 = "abcd efgh"
+length = len(s1)
+slice1 = s1[length::-1]
+words = slice1.split()
+words = list(reversed(words))
+print(' '.join(words))
+
+strSample, strSample2 = 'a1bcd', 'hgf!e'
+
+
+listSample, listSample2 = list(strSample), list(strSample2)
+
+i = 0
+j = len(listSample) - 1
+
+while i < j:
+    if not listSample[i].isalpha():
+        i += 1
+    elif not listSample[j].isalpha():
+        j -= 1
+    else:
+        # swap the element in the list
+        # if both elements are alphabets
+        listSample[i], listSample[j] = listSample[j], listSample[i]
+        i += 1
+        j -= 1
+
+# convert list into string
+# by concatinating each element in the list
+strOut = ''.join(listSample)
+strOut2 = ''.join(listSample2)
+print(strOut, strOut2)
+
+
+
+
+
